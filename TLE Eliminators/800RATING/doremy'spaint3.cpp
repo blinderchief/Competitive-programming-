6d438f6c2e9fd60e8b61551ebb3745e204208671
SuyashSingh011 <suyashsingh.raebareli@gmail.com>
2024-03-26 15:03:40 +0530

    my commit

---
+++ TLE Eliminators/800RATING/doremy'spaint3.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int tt; cin>>tt;
+  while(tt--){
+    int n; cin>>n;
+    map<int,int> mp;
+    vector<int> v;
+    int x =0;
+    for(int i=0;i<n;i++){
+      cin>>x;
+      mp[x]++;
+    }
+    if(mp.size()>=3) {
+      cout<<"NO"<<'\n';
+      continue;
+      }
+
+    if(mp.size()==1) 
+    {
+      cout<<"YES"<<'\n';
+      continue;
+      }
+    for( auto it : mp) {
+        v.push_back(it.second);
+    }
+    if(abs(v[0]-v[1])>=2)
+      cout<<"NO"<<'\n';
+    else cout<<"YES"<<'\n';
+    
+
+    }
+  
+    
+  }
