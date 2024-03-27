9502c5c5b5133bb9e691f9a2de9dad59609a07fe
SuyashSingh011 <suyashsingh.raebareli@gmail.com>
2024-03-27 12:07:57 +0530

    first commit

--- TLE Eliminators/800RATING/coins.cpp
+++ TLE Eliminators/800RATING/coins.cpp
@@ -1,32 +1,5 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// #define ll long long int 
-// int main(){
-//   int t; cin>>t;
-//   while(t--){
-//     ll n,k; cin>>n>>k;
-//     int x=0,y=0;
-//     bool res=false;
-//     while(x>0 && y<=9){
-//       x = (n-(k*y));
-//       if(x%2==0){
-//         res =true;
-//         break;
-//       }
-//       else{
-//       y++;
-//       }
-//     }
-//     if(res) cout<<"YES"<<'\n';
-//     else cout<<"NO"<<'\n';
-
-
-//   }
-// }
 #include<bits/stdc++.h>
 using namespace std;
-
-
 int main() {
     int t; 
     cin >> t;
