8d18ee77a6edf314a07f43505a523aaadf3c7c96
SuyashSingh011 <suyashsingh.raebareli@gmail.com>
2024-04-16 00:02:00 +0530

    first commit

---
+++ TLE Eliminators/900Rating/badboy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+int main() {
+int t; cin>>t;
+while(t--){
+  int n,m,i,j; cin>>n>>m>>i>>j;
+  cout<<1<<' '<<1<<' '<<n<<' '<<m<<'\n';
+}
+
+return 0;
+}
