a78c8396bd085c9248a36bf1cb3a32e9a454fb5d
SuyashSingh011 <suyashsingh.raebareli@gmail.com>
2025-02-13 21:11:02 +0530

    Suyashkumarsingh

--- new.cpp
+++ new.cpp
@@ -27,6 +27,7 @@ int main() {
         cout<<"hi"<<'\n';
         cout<<"Suyash"<<"\n";
         cout<<"Blinderchief"<<'\n';
+        cout<<"Suyash Kumar Singh"<<"\n";
     }
     auto end = chrono::high_resolution_clock::now();
     auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
