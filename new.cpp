5845c1a89fa966c5dcc2a496ec8af6006e612bd1
SuyashSingh011 <suyashsingh.raebareli@gmail.com>
2025-02-13 18:21:24 +0530

    new.cpp

---
+++ new.cpp
@@ -0,0 +1,31 @@
+/**
+ * writer:blinderchief
+ **/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i,a,n) for(int i = a; i < n; i++)
+#define vll vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define po pop_back
+#define me(a, x) memset(a, x, sizeof(a))
+#define all(v) v.begin(), v.end()
+#define no cout << "NO" << '\n';
+#define yes cout << "YES" << '\n';
+#define sot(v) sort(all(v))
+#define INF (int)1e18
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+int main() {
+    auto begin = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        cout<<"HELLO SUYASH"<<"\n";
+    }
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+   //cout << "Time taken: " << elapsed.count() * 1e-9 << " seconds" << '\n';
+    return 0;
+}
